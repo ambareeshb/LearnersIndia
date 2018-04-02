c4032d9b1806dd1c42d6c7b9bbf32f8b11799252
Ambareesh B <ambareeshb@Ambareeshs-MacBook-Air.local>
2018-04-02 07:35:46 +0530

    video icon fix

--- app/src/main/kotlin/com/gbmainframe/learnersindia/activities/VideoPlayerActivity.kt
+++ app/src/main/kotlin/com/gbmainframe/learnersindia/activities/VideoPlayerActivity.kt
@@ -1,13 +1,12 @@
 package com.gbmainframe.learnersindia.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.webkit.WebSettings
 import android.webkit.WebViewClient
 import com.gbmainframe.learnersindia.R
-import com.vimeo.networking.VimeoClient
-import com.vimeo.networking.VimeoClient.*
+import com.vimeo.networking.VimeoClient.getInstance
 import com.vimeo.networking.callbacks.ModelCallback
 import com.vimeo.networking.model.User
 import com.vimeo.networking.model.Video
@@ -25,25 +24,25 @@ class VideoPlayerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_video_player)
         val uri = "/262504336"
         val path = "https://player.vimeo.com/video"
-//        val uri = intent.extras.get(VIDEO_URI_BUNDLE_ID)
+//      val uri = intent.extras.get(VIDEO_URI_BUNDLE_ID)
 
 
         getInstance().fetchCurrentUser(object : ModelCallback<User>(User::class.java) {
             override fun success(t: User?) {
-                Log.d("VimeoSuccess", "videoGot")
                 val videoUri = t?.videosConnection?.uri
-                Log.d("VIMEO_SUCCESS", "${t?.videoCount}")
                 getInstance().fetchNetworkContent(videoUri + uri, object : ModelCallback<Video>(Video::class.java) {
                     override fun success(video: Video?) {
                         vimeoPlayer.apply {
                             webViewClient = WebViewClient()
-                            settings.apply {
-                                javaScriptEnabled = true
-                                javaScriptCanOpenWindowsAutomatically = true
-                                pluginState = WebSettings.PluginState.ON
-                                builtInZoomControls = true
-                            }
+//                            settings.apply {
+//                                javaScriptEnabled = true
+//                                javaScriptCanOpenWindowsAutomatically = true
+//                                pluginState = WebSettings.PluginState.ON
+//                                builtInZoomControls = true
+//                            }
                             loadData(video?.embed?.html, "text/html", "utf-8")
+                            visibility = View.VISIBLE
+                            learnersProgress.visibility = View.GONE
                         }
                     }
 
